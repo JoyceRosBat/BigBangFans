688e2f612d2fe4e3597306a11befc8de7cf3d29e
Joyce Rosario Batista <joyce.rosbat@gmail.com>
2022-12-08 18:05:49 +0000

    Initial Commit

---
+++ BigBangFans/BigBangFansApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigBangFansApp.swift
+//  BigBangFans
+//
+//  Created by Joyce Rosario Batista on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BigBangFansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
